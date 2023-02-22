e6723d72a8cb06a51b8fa87bd98861f4ca563b01
Russell Dunk <russelldunk@gmail.com>
2023-02-22 13:15:42 -0500

    Cleanish initial state

--- new_attempt.py
+++ new_attempt.py
@@ -22,22 +22,6 @@ def clean(not_list, pos):
     pos = list(filter(lambda x: not set(x) & set(not_list), pos))
     return pos
 
-guess1 = [Person.Green, Weapon.Pipe, Room.Hall]
-guess2 = [Person.Scarlett, Weapon.Pipe, Room.Kitchen]
-guess3 = [Person.Plum, Weapon.Knife, Room.Dining]
-guess4 = [Person.Green, Weapon.Wrench, Room.Lounge]
-guess5 = [Person.Green, Weapon.Pipe, Room.Study]
-guess6 = [Person.Plum, Weapon.Candlestick, Room.Library]
-guess7 = [Person.White, Weapon.Rope, Room.Kitchen]
-guess8 = [Person.Peacock, Weapon.Rope, Room.Hall]
-guess9 = [Person.White, Weapon.Wrench, Room.Library]
-guess10 = [Person.Green, Weapon.Pipe, Room.Billiard]
-guess11 = [Person.Green, Weapon.Candlestick, Room.Billiard]
-guess12 = [Person.Mustard, Weapon.Candlestick, Room.Dining]
-guess13 = [Person.Green, Weapon.Knife, Room.Ballroom]
-guess14 = [Person.Peacock, Weapon.Rope, Room.Study]
-guess15 = [Person.Green, Weapon.Pipe, Room.Conservatory]
-
 
 sc = [
     list(Room),
@@ -46,9 +30,19 @@ sc = [
 ]
 
 p11 = Person.Mustard
-p12 = Room.Hall
-p13 = Weapon.Candlestick
+p12 = Weapon.Rope
+p13 = Person.Scarlett
 p1_list = [p11, p12, p13]
+
+guess1 = [Person.White, Weapon.Revolver, Room.Library]
+
+s_not = tuple([])
+p2_not = tuple([])
+p3_not = tuple([*guess1])
+p4_not = tuple([])
+p5_not = tuple([])
+p6_not = tuple([])
+
 
 p1c = [
     [p11],
@@ -56,54 +50,37 @@ p1c = [
     [p13],
 ]
 p2c = [
-    [Room.Study],
-    guess14,
-    [Room.Conservatory],
+    [Person.Green],
+    [Room.Hall],
+    [Weapon.Revolver],
 ]
-
 p3c = [
-    guess4,
-    guess6,
-    guess9,
 ]
 p4c = [
-    guess2,
+    guess1,
 ]
 p5c = [
-    guess3,
-    guess7,
-    guess13,
 ]
 p6c = [
-    [Room.Billiard],
-    guess11,
 ]
 
-s_not = tuple([])
-p2_not = tuple([*guess4, *guess9, *guess10])
-p3_not = tuple([*guess1, *guess10, *guess11])
-p4_not = tuple([*guess1, *guess10, *guess11, *guess12])
-p5_not = tuple([*guess1, *guess10, *guess11, *guess12])
-p6_not = tuple([*guess1, *guess8, *guess12, *guess14])
 
-p6 = None
-p5 = None
 s = () 
 count = 0
 
-dex = 0
+
 prob = dict()
-print(len(list(player(p1_list + list(s_not), sc))))
+progress = 0
+total_solutions = len(list(player(p1_list + list(s_not), sc)))
 for p1 in player([], p1c):
     for s in player(p1 + s_not, sc):
-        dex += 1
-        print(dex)
+        progress += 1
+        print(int((progress/total_solutions)*100))
         for p2 in player(s + p1 + p2_not, p2c):
             for p3 in player(s + p1 + p2 + p3_not, p3c):
                 for p4 in player(s + p1 + p2 + p3 + p4_not, p4c):
                     for p5 in player(s + p1 + p2 + p3 + p4 + p5_not, p5c):
                         for p6 in player(s + p1 + p2 + p3 + p4 + p5 + p6_not, p6c):
-                            pass
                             # if str(s[0]) not in prob:
                             #     prob[str(s[0])] = 0
                             # prob[str(s[0])] += 1
@@ -117,10 +94,7 @@ for p1 in player([], p1c):
                             if total_key not in prob:
                                 prob[total_key] = 0
                             prob[total_key] += 1
-
                             count += 1
-                            if count % 1000000 == 0:
-                                print(p1, s, p2, p3, p4, p5, p6)
 
 for k in sorted(prob, key=prob.get, reverse=True):
     print((prob[k] / count).__format__("0.3f"), k)
